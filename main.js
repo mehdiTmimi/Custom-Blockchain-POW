577cdb33fada504df0be1e9df911fab171bd7e0e
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-03-05 15:42:50 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const crypto = require('crypto');
+const { generateHash,sign, generateKeyPairs, verifyCustom, signCustom, generateKeyPairsCustom, generateHashCustom } = require('./utils');
+// 1- generate Hash
+console.log("hash de salut est : " , generateHashCustom("salut"));
+// 2- generate keyPairs
+const { publicKey, privateKey } = generateKeyPairsCustom()
+// 3- sign message with private key
+const data = "salut uemf 2024s"
+const signature = signCustom(data,privateKey)
+console.log(signature);
+// 4- verify
+const isVerified = verifyCustom(data,publicKey,signature)
+console.log(isVerified);
