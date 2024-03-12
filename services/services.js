66e5b64a89c2121ffc1a01fc592244706a236dc2
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-03-12 16:02:37 +0000

    ok

--- services/services.js
+++ services/services.js
@@ -3,35 +3,34 @@ const BlockChain = require("../models/blockchain")
 const Bloc = require("../models/bloc")
 const Transaction = require("../models/transaction")
 const TransactionReward = require("../models/transactionReward")
-const load = (path) => {
+const load = async (path) => {
     //returns blockchain
-    fs.readFile(path, (err, data) => {
-        if (err)
-            return console.log(err, "erreur dans la lecture du fichier")
-        data = JSON.parse(data).blockchain
+    let data = await fs.promises.readFile(path)
 
-        const blockchain = new BlockChain(data.name, data.blockReward, data.difficuly,
-            data.cyrptoFunction, data.proofStyle)
+    data = JSON.parse(data).blockchain
 
-        data.blocs.forEach(element => {
-            const transactionReward = new TransactionReward(element.transactionReward.sender,
-                element.transactionReward.amount, element.transactionReward.signture)
-            const transactions = element.transactions.map(tx => {
-                return new Transaction(tx.sender, tx.receipient, tx.amount, tx.fees, tx.signture)
-            })
-            const bloc = new Bloc(element.peviousHash, element.nonce, element.height,
-                element.hash, transactionReward, transactions)
-            transactionReward.bloc = cloc
-            transactions.forEach(tx => tx.bloc = bloc)
+    const blockchain = new BlockChain(data.name, data.blockReward, data.difficuly,
+        data.cyrptoFunction, data.proofStyle)
 
-            bloc.previous = blockchain.lastBlock
-            blockchain.lastBlock = bloc
+    data.blocs.forEach(element => {
+        const transactionReward = new TransactionReward(element.transactionReward.sender,
+            element.transactionReward.amount, element.transactionReward.signture)
+        const transactions = element.transactions.map(tx => {
+            return new Transaction(tx.sender, tx.receipient, tx.amount, tx.fees, tx.signture)
         })
-        return blockchain
+        const bloc = new Bloc(element.peviousHash, element.nonce, element.height,
+            element.hash, transactionReward, transactions)
+        transactionReward.bloc = bloc
+        transactions.forEach(tx => tx.bloc = bloc)
+
+        bloc.previous = blockchain.lastBlock
+        blockchain.lastBlock = bloc
     })
+    return blockchain
+
 
 }
-load("../bd/blockchain_v1.json")
+load("../bd/blockchain_v1.json").then(res=>console.log(res));
 
 
 
@@ -73,7 +72,7 @@ const save = (blockchain, path) => {
         tmpBlocs.push(bloc)
         tete = tete.previous
     }
-    data.blockchain.blocs=tmpBlocs.reverse()
+    data.blockchain.blocs = tmpBlocs.reverse()
     fs.writeFile(path, JSON.stringify(data), (err) => {// data ??
         if (err)
             console.log("eerreur ecriture du fichier")
