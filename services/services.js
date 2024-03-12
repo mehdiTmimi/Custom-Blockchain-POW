ac88f9629c302bef98d381fa9b83ad3f9067d137
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-03-12 15:04:46 +0000

    ok

--- services/services.js
+++ services/services.js
@@ -1,5 +1,8 @@
 const fs = require("fs")
 const BlockChain = require("../models/blockchain")
+const Bloc = require("../models/bloc")
+const Transaction = require("../models/transaction")
+const TransactionReward = require("../models/transactionReward")
 const load = (path)=>{
     //returns blockchain
     fs.readFile(path,(err,data)=>{
@@ -9,7 +12,21 @@ const load = (path)=>{
        
         const blockchain = new BlockChain(data.name,data.blockReward,data.difficuly,
             data.cyrptoFunction,data.proofStyle)
-        console.log(blockchain)   
+      
+        data.blocs.forEach(element=>{
+            const transactionReward = new TransactionReward(element.transactionReward.sender,
+                element.transactionReward.amount,element.transactionReward.signture)
+            const transactions = element.transactions.map(tx=>{
+                return new Transaction(tx.sender,tx.receipient,tx.aamout,tx.fees,tx.signture)
+            })
+            const bloc = new Bloc(element.peviousHash,element.nonce,element.height,
+                element.hash,transactionReward,transactions)
+            transactionReward.bloc=cloc
+            transactions.forEach(tx=>tx.bloc=bloc)
+            
+            bloc.previous=blockchain.lastBlock
+            blockchain.lastBlock=bloc
+        })
     })
 }
 load("../bd/blockchain_v1.json")
