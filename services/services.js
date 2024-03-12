0b266b9fcb74f3208cb55a6f7b7cd9f96bf620fe
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-03-12 14:14:29 +0000

    ok

--- services/services.js
+++ services/services.js
@@ -1,6 +1,22 @@
+const fs = require("fs")
+const BlockChain = require("../models/blockchain")
 const load = (path)=>{
     //returns blockchain
+    fs.readFile(path,(err,data)=>{
+        if(err)
+            return console.log(err,"erreur dans la lecture du fichier")
+        data = JSON.parse(data).blockchain
+       
+        const blockchain = new BlockChain(data.name,data.blockReward,data.difficuly,
+            data.cyrptoFunction,data.proofStyle)
+        console.log(blockchain)   
+    })
 }
+load("../bd/blockchain_v1.json")
+
+
+
+
 const save=(blockchain,path)=>{
 
 }
