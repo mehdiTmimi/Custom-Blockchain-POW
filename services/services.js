07a91c8718cbc5393c204011c352ebe333d9cce1
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-03-12 13:50:43 +0000

    ok

---
+++ services/services.js
@@ -0,0 +1,30 @@
+const load = (path)=>{
+    //returns blockchain
+}
+const save=(blockchain,path)=>{
+
+}
+const solde = (address)=>{
+    //returns amount of coins (float)
+}
+const verifierTransaction=(transaction)=>{
+    //returs true or flase
+}
+const verifierBloc = (bloc)=>{
+    //returs true or flase
+}
+const ajouterBloc=(blockchain,bloc)=>{
+    //returs true or flase
+}
+const verifierBlockchain = (blockchain)=>{
+    //returs true or flase
+}
+module.exports={
+    load,
+    save,
+    solde,
+    verifierBloc,
+    verifierBlockchain,
+    verifierTransaction,
+    ajouterBloc
+}
